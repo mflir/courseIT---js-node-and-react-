6afa69b0c8fe8787d91f9eccde6cb483f67c0ac1
mflir <scavone.mf@gmail.com>
2019-06-11 22:03:39 -0300

    create header and search

---
+++ prueba-react/src/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Search from './Search'
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <div>Hola Header</div>
+        <Search />
+      </div>
+    )
+  }
+
+}
+
+export default Header;
